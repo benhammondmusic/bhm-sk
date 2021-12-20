736908bda0ff40f1e66edd8dc001bdbfbdd74a26
Ben Hammond <benjamin.hammond@gmail.com>
2021-12-19 19:44:45 -0700

    initial setup

---
+++ src/routes/booking.svelte
@@ -0,0 +1,24 @@
+<svelte:head>
+	<title>Booking</title>
+</svelte:head>
+
+<section>
+<h1>Booking</h1>
+<p>123</p>
+</section>
+
+<style>
+	section {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		flex: 1;
+	}
+
+	h1 {
+		width: 100%;
+	}
+
+
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -12,21 +12,14 @@
 
 <section>
 	<h1>
-		<div class="welcome">
-			<picture>
-				<source srcset="svelte-welcome.webp" type="image/webp" />
-				<img src="svelte-welcome.png" alt="Welcome" />
-			</picture>
-		</div>
 
-		to your new<br />SvelteKit app
+		Ben Hammond Music
 	</h1>
 
 	<h2>
-		try editing <strong>src/routes/index.svelte</strong>
+		Song Builder
 	</h2>
 
-	<Counter />
 </section>
 
 <style>
@@ -42,18 +35,5 @@
 		width: 100%;
 	}
 
-	.welcome {
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
 
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
-	}
 </style>
