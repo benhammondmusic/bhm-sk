fb666b81b7f162a70c9993b49e9347dc6fb68708
Ben Hammond <benjamin.hammond@gmail.com>
2022-01-08 13:34:54 -0700

    moves some text over

--- src/routes/booking.svelte
+++ src/routes/booking.svelte
@@ -4,7 +4,52 @@
 
 <section>
 <h1>Booking</h1>
-<p>123</p>
+<p>
+	As a solo artist, I specialize in destination weddings and other intimate events. Often
+	incorporating loop pedals and extended performance techniques, I truly enjoy providing the energy
+	and variety of a full band or DJ with the pricing and space considerations of a single performer.
+	If a more intricate production is in your plans, I also work with an amazing assortment of band
+	members including horns, strings, percussion, and much more.
+</p>
+
+<p>
+	My performances are improvised and passionate, shaping themselves in real-time to suit you and
+	your guests, whether that be a romantic ceremony, relaxing over cocktails, or dancing the night
+	away! My services are well suited for clients who are passionate about live music, and who are
+	seeking a fun and memorable way to enhance their celebration. Wherever you might be in your
+	planning, please <a href="mailto:hello@benhammondmusic.com">email</a> for more info, or receive a
+	custom quote by filling in my <a href="contact.html#dubsado_lead_capture">event form</a>. Let's
+	make some music!
+</p>
+
+<ul class="bullet">
+	<lh>Event Package: starting at $1,500</lh>
+	<li>
+		live music including romantic acoustic repertoire, upbeat live-looped dance sets, and everything
+		in between
+	</li>
+	<li>additional DJ coverage, split before and after live music</li>
+	<li>learning special requests for processional and 1st dance</li>
+	<li>unlimited requests from artist’s 1,000+ song list and DJ library</li>
+	<li>best for: welcome dinners, ceremony/cocktails, and elopements</li>
+</ul>
+
+<ul class="bullet">
+	<lh>Additional Coverage</lh>
+	<li>can be pro-rated in half hour increments</li>
+	<li>best for: longer events, micro weddings</li>
+</ul>
+
+<ul class="bullet">
+	<lh>Expanded Ensembles</lh>
+	<li>options: cello, percussion, violin, horns, 2nd vocalist, and more</li>
+</ul>
+
+<small>
+	*discounts may be available for smaller, informal events in the Denver metro, and additional fees
+	may apply for events more than 2 hours from Denver; please inquire for a specific quote
+</small>
+
 </section>
 
 <style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -19,6 +19,20 @@
 		Song Builder
 	</h2>
 
+<p>
+	"Funky… guitar-looping, beat-boxing, soul-singing… powerhouse Ben Hammond" (Sarasota Herald
+	Tribune) performs a wide range of groove-oriented acoustic mashups, original compositions and
+	re-imagined cover songs.
+</p>
+
+<p>
+	For over two decades, Hammond has honed his diverse, energetic style and is consistently one of
+	the most sought after musicians in those places he's called home, including most recently the
+	Colorado Rocky Mountains and Front Range. As noted by the Portland Press Herald: "Hammond is a
+	versatile musician who writes memorable originals, delivers when he decides to cover someone, and
+	uses his guitar, voice and gadgets to create a sound that you want to keep hearing."
+</p>
+
 </section>
 
 <style>
