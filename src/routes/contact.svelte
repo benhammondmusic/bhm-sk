f8ef326f840508838a2b0d43f9c14cbd8669198c
Ben Hammond <benjamin.hammond@gmail.com>
2022-01-08 22:11:05 -0700

    renders songs

--- src/routes/contact.svelte
+++ src/routes/contact.svelte
@@ -3,40 +3,39 @@
 </svelte:head>
 
 <section>
-<h1>Contact</h1>
-<p>
-	Thanks for visiting! I'd love to connect on <a href="https://www.facebook.com/benhammondmusicpage"
-		>Facebook</a
-	>,
-	<a href="https://www.instagram.com/benhammondmusic">Instagram,
-	<a href="https://www.linkedin.com/in/benhammondmusic/">LinkedIn
-	and <a href="https://twitter.com/benhammondmusic">Twitter.
-</p>
-<p>
-	Interested in live music for your event or concert series? <a
-		href="mailto:hello@benhammondmusic.com">Send me an email</a
-	> to receive availability and a custom quote. I look forward to hearing from you!
-</p>
-<p>-Ben</p>
+	<h1>Contact</h1>
+	<p>
+		Thanks for visiting! I'd love to connect on
+		<a href="https://www.facebook.com/benhammondmusicpage">Facebook</a>,
+		<a href="https://www.instagram.com/benhammondmusic">Instagram</a>,
+		<a href="https://www.linkedin.com/in/benhammondmusic/"> LinkedIn</a> and
+		<a href="https://twitter.com/benhammondmusic">Twitter</a>.
+	</p>
+	<p>
+		Interested in live music for your event or concert series?
+		<a href="mailto:hello@benhammondmusic.com">Send me an email</a>
+		to receive availability and a custom quote. I look forward to hearing from you!
+	</p>
+	<p>-Ben</p>
 
-<p>
-	<a href="https://www.facebook.com/benhammondmusicpage" title="Facebook"
-		><i class="fa fa-facebook" aria-hidden="true" />Facebook</a
-	>
+	<p>
+		<a href="https://www.facebook.com/benhammondmusicpage" title="Facebook"
+			><i class="fa fa-facebook" aria-hidden="true" />Facebook</a
+		>
 
-	<a href="https://www.instagram.com/benhammondmusic" title="Instagram"
-		><i class="fa fa-instagram" aria-hidden="true" />Instagram</a
-	>
+		<a href="https://www.instagram.com/benhammondmusic" title="Instagram"
+			><i class="fa fa-instagram" aria-hidden="true" />Instagram</a
+		>
 
-	<a href="https://twitter.com/benhammondmusic" title="Twitter"
-		><i class="fa fa-twitter" aria-hidden="true" />Twitter</a
-	>
+		<a href="https://twitter.com/benhammondmusic" title="Twitter"
+			>
+			<i class="fa fa-twitter" aria-hidden="true" />Twitter</a
+		>
 
-	<a href="https://www.linkedin.com/in/benhammondmusic" title="LinkedIn"
-		><i class="fab fa-linkedin-in" aria-hidden="true" />LinkedIn</a
-	>
-</p>
-
+		<a href="https://www.linkedin.com/in/benhammondmusic" title="LinkedIn">
+			<i class="fab fa-linkedin-in" aria-hidden="true" />LinkedIn
+		</a>
+	</p>
 </section>
 
 <style>
@@ -51,6 +50,4 @@
 	h1 {
 		width: 100%;
 	}
-
-
 </style>
