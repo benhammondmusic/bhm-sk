7677344a9ff28f028622cf7f0ab12a111ccc184a
Ben Hammond <benjamin.hammond@gmail.com>
2022-01-09 13:12:26 -0700

    new page names

---
+++ src/routes/covers.svelte
@@ -0,0 +1,55 @@
+<script>
+	import { songs } from '../data/songlist.json';
+</script>
+
+<svelte:head>
+	<title>Song List</title>
+</svelte:head>
+
+<section>
+	<h1>Song List</h1>
+
+	<!-- LIST OF ALL SONGS -->
+	<div class="songs">
+		{#each songs as song}
+			<article class="songItem">
+				<!-- EACH SONG'S TITLE -->
+				{song.title} -
+				<!-- EACH SONG'S ARTIST(S) -->
+				{#each song.artistArray as artist, i}
+					{artist}{i < song.artistArray.length - 1 ? ', ' : ''}
+				{/each}
+			</article>
+		{/each}
+	</div>
+</section>
+
+<style>
+	section {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		flex: 1;
+	}
+
+	h1 {
+		width: 100%;
+	}
+
+	.songs {
+		display: flex;
+		flex-wrap: wrap;
+		justify-content: space-evenly;
+	}
+
+	.songItem {
+		margin: 10px;
+		padding: 15px;
+		background-color: #8899bb;
+		border-radius: 5px;
+		color: #ddeeff;
+		font-weight: 400;
+		font-size: 1.3rem;
+	}
+</style>
