23f595e46dc6894a5f0e2b852786445ea164197a
Ben Hammond <benjamin.hammond@gmail.com>
2022-01-09 18:43:57 -0700

    moving

--- src/routes/originals.svelte
+++ src/routes/originals.svelte
@@ -3,8 +3,122 @@
 </svelte:head>
 
 <section>
-<h1>Music</h1>
-<p>123</p>
+	<h1>Music</h1>
+	<p>123</p>
+
+	<article class="album">
+		<img src="/images/albumcovers/gone.jpg" alt="Album Art for Gone" class="albumArt" />
+
+		<div class="albumText">
+			<h2 class="albumTitle">Gone (To California)</h2>
+			<span class="d-block mb-2 text-white-opacity-05">Video and Single - 2017</span>
+			<p class="mb-4">
+				Featuring <a href="https://www.johnpatti.com">John Patti</a>. Video by
+				<a href="https://joey-durango.com/">Joey Durango</a>. Song by
+				<a href="https://www.facebook.com/matt.gerhardt.16">Matt Gerhardt</a>.
+			</p>
+			<span class="buy-icons">
+				<a href="https://open.spotify.com/album/0wO71kn47uK9AA5J4NqbN9" title="Spotify">
+					<i class="fa fa-spotify" aria-hidden="true" />
+				</a>
+				<a
+					href="https://music.apple.com/us/album/gone-to-california-single/1231607849"
+					title="Apple Music"
+					><i class="fa fa-apple" aria-hidden="true" />
+				</a>
+				<a href="https://www.amazon.com/dp/B071YQK9BL/ref=ap_ws_tlw_trk2" title="Amazon Music"
+					><i class="fa fa-amazon" aria-hidden="true" />
+				</a>
+			</span>
+		</div>
+	</article>
+
+	<article class="album">
+		<img src="/images/albumcovers/friction.jpg" alt="Album Art for Friction" class="albumArt" />
+
+		<div class="albumText">
+			<h2 class="albumTitle">Friction</h2>
+			<span class="d-block mb-2 text-white-opacity-05">Studio Album - 2014</span>
+			<p class="mb-4">
+				Co-Produced with <a href="https://www.thebirdtribe.com/">Matt Frost</a>. Full Band Originals
+				and Solo, Live-Looped Covers ft.
+				<a href="https://www.youtube.com/user/kindredsheeler/videos">Kindred</a> and many more.
+			</p>
+			<span class="buy-icons">
+				<a href="https://open.spotify.com/album/36tOKgpZuT9jiVnCBHxhWO" title="Listen on Spotify"
+					><i class="fa fa-spotify" aria-hidden="true" /></a
+				>
+				<a href="https://music.apple.com/us/album/friction/920576506" title="Listen on Apple Music"
+					><i class="fa fa-apple" aria-hidden="true" /></a
+				>
+				<a href="https://www.amazon.com/Friction-Ben-Hammond/dp/B00NP82T0I/" title="Amazon Music"
+					><i class="fa fa-amazon" aria-hidden="true" /></a
+				>
+			</span>
+		</div>
+	</article>
+
+	<article class="album">
+		<img
+			src="/images/albumcovers/solo.jpg"
+			alt="Album Art for Solo at Stone Mountain"
+			class="albumArt"
+		/>
+
+		<div class="albumText">
+			<h2 class="albumTitle">Solo at Stone Mt Arts Center</h2>
+			<span class="d-block mb-2 text-white-opacity-05">Live Album and DVD - 2011</span>
+			<p class="mb-4">
+				Recorded live, minutes from Ben's hometown in rural Maine. Features solo originals and cover
+				songs. Video by Michael Dana.
+			</p>
+			<span class="buy-icons">
+				<a href="https://open.spotify.com/album/3DTF0fum9uhqGxOS2czdaf" title="Spotify"
+					><i class="fa fa-spotify" aria-hidden="true" /></a
+				>
+				<a
+					href="https://music.apple.com/us/album/solo-at-stone-mt-arts-center/417555127"
+					title="Apple Music"><i class="fa fa-apple" aria-hidden="true" /></a
+				>
+				<a
+					href="https://www.amazon.com/Solo-Stone-Mt-Arts-Center/dp/B004MFOCQW/"
+					title="Amazon Music"><i class="fa fa-amazon" aria-hidden="true" /></a
+				>
+			</span>
+		</div>
+	</article>
+
+	<article class="album">
+		<img
+			src="/images/albumcovers/reasonablyhonest.jpg"
+			alt="Album Art for Reasonably Honest"
+			class="albumArt"
+		/>
+
+		<div class="albumText">
+			<h2 class="albumTitle">[Reasonably] Honest</h2>
+			<span class="d-block mb-2 text-white-opacity-05">Studio Album - 2008</span>
+
+			<p class="mb-4">
+				Co-Produced with <a href="https://mojitomastering.com/">Reuben Ghose</a>. Original songs ft.
+				<a href="https://www.zachfinkelstein.com/">Zach Finkelstein</a>,
+				<a href="https://www.jonathanlindhorst.com/">Jon Lindhorst</a>, and more.
+			</p>
+			<span class="buy-icons">
+				<a href="https://open.spotify.com/album/4P8v3P5so2Re7e2JcvuluA" title="Spotify"
+					><i class="fa fa-spotify" aria-hidden="true" /></a
+				>
+				<a
+					href="https://music.apple.com/us/album/reasonably-honest-extended-edition/350749973"
+					title="Apple Music"><i class="fa fa-apple" aria-hidden="true" /></a
+				>
+				<a
+					href="https://www.amazon.com/Reasonably-Honest-Ben-Hammond/dp/B0038AN416/"
+					title="Amazon Music"><i class="fa fa-amazon" aria-hidden="true" /></a
+				>
+			</span>
+		</div>
+	</article>
 </section>
 
 <style>
@@ -19,6 +133,4 @@
 	h1 {
 		width: 100%;
 	}
-
-
 </style>
