421bc4e1d5b2d76c3525d9cba7e3af7caac9104f
Ben Hammond <benjamin.hammond@gmail.com>
2022-01-09 21:28:12 -0700

    adds imgs

--- src/routes/originals.svelte
+++ src/routes/originals.svelte
@@ -6,13 +6,15 @@
 	<h1>Music</h1>
 	<p>123</p>
 
+	<!-- ORIGINAL ALBUMS / RELEASES  -->
+
 	<article class="album">
 		<img src="/images/albumcovers/gone.jpg" alt="Album Art for Gone" class="albumArt" />
 
-		<div class="albumText">
+		<div class="albumInfoBox">
 			<h2 class="albumTitle">Gone (To California)</h2>
-			<span class="d-block mb-2 text-white-opacity-05">Video and Single - 2017</span>
-			<p class="mb-4">
+			<span class="albumYearInfo">Video and Single - 2017</span>
+			<p class="albumDescription">
 				Featuring <a href="https://www.johnpatti.com">John Patti</a>. Video by
 				<a href="https://joey-durango.com/">Joey Durango</a>. Song by
 				<a href="https://www.facebook.com/matt.gerhardt.16">Matt Gerhardt</a>.
@@ -36,10 +38,10 @@
 	<article class="album">
 		<img src="/images/albumcovers/friction.jpg" alt="Album Art for Friction" class="albumArt" />
 
-		<div class="albumText">
+		<div class="albumInfoBox">
 			<h2 class="albumTitle">Friction</h2>
-			<span class="d-block mb-2 text-white-opacity-05">Studio Album - 2014</span>
-			<p class="mb-4">
+			<span class="albumYearInfo">Studio Album - 2014</span>
+			<p class="albumDescription">
 				Co-Produced with <a href="https://www.thebirdtribe.com/">Matt Frost</a>. Full Band Originals
 				and Solo, Live-Looped Covers ft.
 				<a href="https://www.youtube.com/user/kindredsheeler/videos">Kindred</a> and many more.
@@ -65,10 +67,10 @@
 			class="albumArt"
 		/>
 
-		<div class="albumText">
+		<div class="albumInfoBox">
 			<h2 class="albumTitle">Solo at Stone Mt Arts Center</h2>
-			<span class="d-block mb-2 text-white-opacity-05">Live Album and DVD - 2011</span>
-			<p class="mb-4">
+			<span class="albumYearInfo">Live Album and DVD - 2011</span>
+			<p class="albumDescription">
 				Recorded live, minutes from Ben's hometown in rural Maine. Features solo originals and cover
 				songs. Video by Michael Dana.
 			</p>
@@ -95,11 +97,11 @@
 			class="albumArt"
 		/>
 
-		<div class="albumText">
+		<div class="albumInfoBox">
 			<h2 class="albumTitle">[Reasonably] Honest</h2>
-			<span class="d-block mb-2 text-white-opacity-05">Studio Album - 2008</span>
+			<span class="albumYearInfo">Studio Album - 2008</span>
 
-			<p class="mb-4">
+			<p class="albumDescription">
 				Co-Produced with <a href="https://mojitomastering.com/">Reuben Ghose</a>. Original songs ft.
 				<a href="https://www.zachfinkelstein.com/">Zach Finkelstein</a>,
 				<a href="https://www.jonathanlindhorst.com/">Jon Lindhorst</a>, and more.
@@ -119,6 +121,59 @@
 			</span>
 		</div>
 	</article>
+
+	<!-- PRESS REVIEWS SLIDER -->
+
+	<article class="pressReview">
+		<img
+			class="reviewImage"
+			src="images/reviewers/portlandpressherald.png"
+			alt="Portland Press Herald Logo"
+		/>
+		<h3>Portland Press Herald</h3>
+		<div class="reviewer">Aimsel Ponti</div>
+		<p class="review">
+			&ldquo;&nbsp; Hammond is a versatile musician who writes memorable originals, delivers when he
+			decides to cover someone, and uses his guitar, voice and gadgets to create a sound that you
+			want to keep hearing. &nbsp;&ldquo;
+		</p>
+	</article>
+
+	<article class="pressReview">
+		<img
+			class="reviewImage"
+			src="images/reviewers/ticket.jpg"
+			alt="Sarasota Herald Tribune Ticket Logo"
+		/>
+		<h3>Sarasota Herald Tribune (Ticket)</h3>
+		<div class="reviewer">Dawn Scire</div>
+		<p class="review">
+			&ldquo;&nbsp; one minute he's singer-songwriter smooth, the next breaking into rap-jazzy
+			scats, skillfully co-merging genres within songs. &nbsp;&ldquo;
+		</p>
+	</article>
+
+	<article class="pressReview">
+		<img class="reviewImage" src="images/reviewers/moss.jpg" alt="Moss Logo" />
+		<h3>Jordan Johnson</h3>
+		<div class="reviewer">Owner of the venue 'Moss' in Denver</div>
+		<p class="review">
+			&ldquo;&nbsp; He was awesome! His looping and talent with an array of instruments allows him
+			to play anything from background music to dance party... a talented musician and a great guy,
+			highly recommended for a wedding or any other special event. &nbsp;&ldquo;
+		</p>
+	</article>
+
+	<article class="pressReview">
+		<img class="reviewImage" src="images/reviewers/raneelee.jpg" alt="Ranee Lee Headshot" />
+		<h3>Ranee Lee</h3>
+		<div class="reviewer">Award Winning Jazz Vocalist</div>
+		<p class="review">
+			&ldquo;&nbsp; His beautiful lyrics, ranging from sensual to exuberance and everything in
+			between come together nicely in a great package of vocal performance and instrumental gifts.
+			&nbsp;&ldquo;
+		</p>
+	</article>
 </section>
 
 <style>
@@ -132,5 +187,9 @@
 
 	h1 {
 		width: 100%;
+	}
+
+	.reviewImage {
+		max-width: 200px;
 	}
 </style>
